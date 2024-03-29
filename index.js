3e713293d96a60c32fc5c06037949f4c3861322b
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-03-29 20:39:10 +0100

    configuration de passport-local

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const passportLocal = require('passport-local');
 const usersRouter = require("./routes/users");
 const ordinateursRouter = require("./routes/ordinateurs");
+const prisma = require("./db/prisma");
+
+// Recuperation de l'objet de la stratégie locale
+const LocalStrategy = passportLocal.Strategy;
+
 
 dotenv.config();
 const PORT = process.env.PORT;
 const server = express();
 server.use(express.json());
+
+// Configuration de passport : il attends deux arguments qui sont 
+// les champs d'authentification et le callback de vérification
+passport.use(new LocalStrategy({ usernameField : "email", passwordField : "password" }, async (email, password, done) => {
+  // Vérifier d'abord que l'utilisateur existe
+  try {
+    const user = await prisma.user.findFirst({
+      where : {
+        email
+      }
+    });
+
+    if (!user) {  // Si l'utilisateur n'est pas trouver on renvoie le vide
+      return done(null, false)
+    }
+
+    bcrypt.compare(password, user.password, (err, isMatch) => { // S'il existe on compare le mot de passe
+      if (err) {
+        return done(null, false, err)
+      }
+      if (!isMatch) {
+        return done(null, false, { message : 'Mot de passe incorrect' })
+      }
+      return done(null, user)  // On renvoie l'objet user si aucune erreur n'est rencontrée
+    })
+  } catch (error) {
+    return res.status(500)
+  }
+}))
+
+
 
 server.get("/", (req, res) => {
   res.send("DEV WEB C3");
